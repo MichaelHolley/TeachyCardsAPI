10f58c70c66d9c176f11a731517f2f5a7b15280a
Michael Holley <michael.philipp.holley@gmail.com>
2021-08-22 11:31:12 +0200

    adjust Application Port

--- TeachyCardsAPI/Program.cs
+++ TeachyCardsAPI/Program.cs
@@ -21,6 +21,7 @@ namespace TeachyCardsAPI
 				.ConfigureWebHostDefaults(webBuilder =>
 				{
 					webBuilder.UseStartup<Startup>();
+					webBuilder.UseUrls("http://localhost:5002");
 				});
 	}
 }
